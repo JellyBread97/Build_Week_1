876a25e75de5d110427b6666f3a78916ec544bbc
MrsOye <flaviabosibori@gmail.com>
2022-10-17 15:52:22 +0200

    save

---
+++ script.js
@@ -0,0 +1,131 @@
+const star1= document.querySelector(".star1");
+const star2= document.querySelector(".star2");
+const star3= document.querySelector(".star3");
+const star4= document.querySelector(".star4");
+const star5= document.querySelector(".star5");
+const star6= document.querySelector(".star6");
+const star7= document.querySelector(".star7");
+const star8= document.querySelector(".star8");
+const star9= document.querySelector(".star9");
+const star10= document.querySelector(".star10");
+
+function stars1(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#00CED1";
+    star3.style.color = "#00CED1";
+    star4.style.color = "#00CED1";
+    star5.style.color = "#00CED1";
+    star6.style.color = "#00CED1";
+    star7.style.color = "#00CED1";
+    star8.style.color = "#00CED1";
+    star9.style.color = "#00CED1";
+    star10.style.color = "#00CED1";
+}
+function stars2(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#FFD700";
+    star3.style.color = "#00CED1";
+    star4.style.color = "#00CED1";
+    star5.style.color = "#00CED1";
+    star6.style.color = "#00CED1";
+    star7.style.color = "#00CED1";
+    star8.style.color = "#00CED1";
+    star9.style.color = "#00CED1";
+    star10.style.color = "#00CED1";
+}
+function stars3(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#FFD700";
+    star3.style.color = "#FFD700";
+    star4.style.color = "#00CED1";
+    star5.style.color = "#00CED1";
+    star6.style.color = "#00CED1";
+    star7.style.color = "#00CED1";
+    star8.style.color = "#00CED1";
+    star9.style.color = "#00CED1";
+    star10.style.color = "#00CED1";
+}
+function stars4(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#FFD700";
+    star3.style.color = "#FFD700";
+    star4.style.color = "#FFD700";
+    star5.style.color = "#00CED1";
+    star6.style.color = "#00CED1";
+    star7.style.color = "#00CED1";
+    star8.style.color = "#00CED1";
+    star9.style.color = "#00CED1";
+    star10.style.color = "#00CED1";
+}
+function stars5(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#FFD700";
+    star3.style.color = "#FFD700";
+    star4.style.color = "#FFD700";
+    star5.style.color = "#FFD700";
+    star6.style.color = "#00CED1";
+    star7.style.color = "#00CED1";
+    star8.style.color = "#00CED1";
+    star9.style.color = "#00CED1";
+    star10.style.color = "#00CED1";
+}
+function stars6(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#FFD700";
+    star3.style.color = "#FFD700";
+    star4.style.color = "#FFD700";
+    star5.style.color = "#FFD700";
+    star6.style.color = "#FFD700";
+    star7.style.color = "#00CED1";
+    star8.style.color = "#00CED1";
+    star9.style.color = "#00CED1";
+    star10.style.color = "#00CED1";
+}
+function stars7(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#FFD700";
+    star3.style.color = "#FFD700";
+    star4.style.color = "#FFD700";
+    star5.style.color = "#FFD700";
+    star6.style.color = "#FFD700";
+    star7.style.color = "#FFD700";
+    star8.style.color = "#00CED1";
+    star9.style.color = "#00CED1";
+    star10.style.color = "#00CED1";
+}
+function stars8(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#FFD700";
+    star3.style.color = "#FFD700";
+    star4.style.color = "#FFD700";
+    star5.style.color = "#FFD700";
+    star6.style.color = "#FFD700";
+    star7.style.color = "#FFD700";
+    star8.style.color = "#FFD700";
+    star9.style.color = "#00CED1";
+    star10.style.color = "#00CED1";
+}
+function stars9(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#FFD700";
+    star3.style.color = "#FFD700";
+    star4.style.color = "#FFD700";
+    star5.style.color = "#FFD700";
+    star6.style.color = "#FFD700";
+    star7.style.color = "#FFD700";
+    star8.style.color = "#FFD700";
+    star9.style.color = "#FFD700";
+    star10.style.color = "#00CED1";
+}
+function stars10(){
+    star1.style.color = "#FFD700";
+    star2.style.color = "#FFD700";
+    star3.style.color = "#FFD700";
+    star4.style.color = "#FFD700";
+    star5.style.color = "#FFD700";
+    star6.style.color = "#FFD700";
+    star7.style.color = "#FFD700";
+    star8.style.color = "#FFD700";
+    star9.style.color = "#FFD700";
+    star10.style.color = "#FFD700";
+}
