863d3aa432cf5ce6b232099e757932d65e16cd7c
MartinKotas <kotasmartin1997@gmail.com>
2022-10-17 14:03:34 +0100

    fixed JS

--- welcome.js
+++ welcome.js
@@ -1,7 +1,7 @@
 function proceed() {
   let checkbox = document.getElementById("checkbox");
   let proceed = document.getElementById("button");
-  proceed.addEventListener("click", function (e) {
+  proceed.addEventListener("click", function () {
     if (checkbox.checked === false) {
       alert("You have to promise first!");
     } else if (checkbox.checked === true) {
@@ -10,6 +10,6 @@ function proceed() {
   });
 }
 
-window.onload = function (e) {
+window.onload = function () {
   proceed();
 };
